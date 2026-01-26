d5b2e8d4265d044046e8d5c64a4ab5a84bdae3f2
henry_liu <2766783861@qq.com>
2026-01-26 20:35:14 +0800

    立芯 FreeRTOS课程跟着的练习 第9讲

---
+++ Core/Inc/my_freertos.h
@@ -0,0 +1,12 @@
+//
+// Created by 1 on 26-1-26.
+//
+
+#ifndef MY_FREERTOS_H
+#define MY_FREERTOS_H
+
+int fputc(int ch, FILE *f) ;
+
+int _write(int file, char *ptr, int len) ;
+
+#endif //MY_FREERTOS_H
